b7e27019ceb1fa590f7b36784726c1ac15e14eb0
Kirk Graves <graves.kirk@gmail.com>
2015-11-26 13:06:26 -0600

    more work with binding

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -1,13 +1,20 @@
 app.controller('ProfileCtrl', ['userFactory', '$firebaseArray', 
 	function(userFactory, $firebaseArray) {
 
-		var self = this;
-		var picsArray = [];
+	var self = this;
+	var picsArray = [];
+	var title;
 
-		picsArray = userFactory.getPicsArray();
+	picsArray = userFactory.getPicsArray().$loaded()
+	.then(function(picsArray) {
 		console.log('picsArray', picsArray);
-		// console.log('pic 1', picsArray[0]);
-		// console.log('pic2', picsArray[1]);
+		console.log('pic0 title', picsArray[0].title);
+		console.log('pic2 description', picsArray[2].description);
+		console.log('pic1 albumName', picsArray[1].albumName);
+		title = picsArray[0].title
+	})
+	.catch(function(error) {
+		console.log('error', error);
+	});		
 
-
-	}]);
+}]);
