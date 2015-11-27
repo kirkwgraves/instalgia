03fde576643ae8b2d27211e085ad77e2e6f55102
Kirk Graves <graves.kirk@gmail.com>
2015-11-27 11:49:20 -0600

    got all pic field data to bind to table

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -2,15 +2,7 @@ app.controller('ProfileCtrl', ['userFactory', '$firebaseArray',
 	function(userFactory, $firebaseArray) {
 
 	var self = this;
-	self.test = 'TEST';
-
-	var userPicObj = {
-		title: null,
-		albumName: null,
-		imageUrl: null,
-		description: null,
-		tags: null
-	}
+	self.picsArray = [];
 
 	userFactory.getPicsArray().$loaded()
 	.then(function(picsArray) {
@@ -18,8 +10,8 @@ app.controller('ProfileCtrl', ['userFactory', '$firebaseArray',
 		console.log('pic0 title', picsArray[0].title);
 		console.log('pic2 description', picsArray[2].description);
 		console.log('pic1 albumName', picsArray[1].albumName);
-		userPicObj = picsArray[0];
-		console.log('userPicObj', userPicObj);
+		self.userPicObj = picsArray[0];
+		console.log('self.userPicObj', self.userPicObj);
 	})
 	.catch(function(error) {
 		console.log('error', error);
