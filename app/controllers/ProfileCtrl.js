25b3724bff5ff730e17db4b79be6612ed10dbaba
Kirk Graves <graves.kirk@gmail.com>
2015-11-29 14:29:46 -0600

    finished base styling for integrated user pic table

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -6,12 +6,13 @@ app.controller('ProfileCtrl', ['userFactory', '$firebaseArray',
 
 	userFactory.getPicsArray().$loaded()
 	.then(function(picsArray) {
-		console.log('picsArray', picsArray);
-		console.log('pic0 title', picsArray[0].title);
-		console.log('pic2 description', picsArray[2].description);
-		console.log('pic1 albumName', picsArray[1].albumName);
-		self.userPicObj = picsArray[0];
-		console.log('self.userPicObj', self.userPicObj);
+		self.picsArray = picsArray;
+		console.log('self.picsArray', self.picsArray);
+		// console.log('pic0 title', picsArray[0].title);
+		// console.log('pic2 description', picsArray[2].description);
+		// console.log('pic1 albumName', picsArray[1].albumName);
+		// self.userPicObj = picsArray[0];
+		// console.log('self.userPicObj', self.userPicObj);
 	})
 	.catch(function(error) {
 		console.log('error', error);
